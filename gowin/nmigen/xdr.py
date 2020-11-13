c62beb14abf985b31ca6ff9ca5bd35888908dfac
Konrad Beckmann <konrad.beckmann@gmail.com>
2020-11-13 17:04:25 +0100

    gowin: Add xdr=2

--- gowin/nmigen/xdr.py
+++ gowin/nmigen/xdr.py
@@ -10,13 +10,17 @@ class BlinkyXDROut(Elaboratable):
     def elaborate(self, platform):
         m = Module()
 
+        # io_dir = "oe"
+        # io_dir = "io"
+        io_dir = "o"
+
         platform.add_resources([
-            Resource("led", 0, Pins("17", dir="oe"),
+            Resource("led", 0, Pins("17"),
                         Attrs(IO_TYPE="LVCMOS33")),
         ])
         
         xdr = self.xdr
-        led = platform.request("led", xdr=xdr)
+        led = platform.request("led", xdr=xdr, dir=io_dir)
 
         clk_freq = platform.default_clk_frequency
         led_counter = Signal(range(int(clk_freq//2)), reset=int(clk_freq//2) - 1)
@@ -25,12 +29,17 @@ class BlinkyXDROut(Elaboratable):
         m.domains += ClockDomain("xdr")
         m.d.comb += ClockSignal(domain="xdr").eq(xdr_clk)
 
-        # Set to True to show the issue with DFF INIT when xdr=1
-        if False:
-            m.d.comb += led.oe.eq(0)
-        else:
+        # Illustrates the issue with DFF INIT when xdr=1
+        # m.d.comb += led.oe.eq(0)
+
+        if io_dir in ("oe", "io"):
             m.d.comb += led.oe.eq(1)
+
+        if xdr <= 1:
             m.d.xdr += led.o.eq(~led.o)
+        elif xdr == 2:
+            m.d.comb += led.o0.eq(1)
+            m.d.comb += led.o1.eq(0)
 
         if xdr > 0:
             m.d.comb += led.o_clk.eq(xdr_clk)
@@ -117,10 +126,19 @@ class BlinkyXDRIn(Elaboratable):
     def elaborate(self, platform):
         m = Module()
 
+        # io_dir = "oe"
+        # io_dir = "io"
+        io_dir = "i"
+
+        platform.add_resources([
+            Resource("btn", 0, Pins("14"),
+                        Attrs(IO_TYPE="LVCMOS33")),
+        ])
+
         xdr = self.xdr
         rgb_led = platform.request("rgb_led")
         
-        btn = platform.request("button", xdr=xdr)
+        btn = platform.request("button", xdr=xdr, dir=io_dir)
 
         clk_freq = platform.default_clk_frequency
         led_counter = Signal(range(int(clk_freq//2)), reset=int(clk_freq//2) - 1)
@@ -141,20 +159,29 @@ class BlinkyXDRIn(Elaboratable):
         # xdr clock high => red led on
         m.d.comb += rgb_led.r.o.eq(ClockSignal("xdr"))
 
-        # Connect button input straight to the green led
-        m.d.comb += rgb_led.g.o.eq(btn.i)
+        if io_dir in ("oe", "io"):
+            m.d.comb += led.oe.eq(0)
+
+        if xdr == 0 or xdr == 1:
+            # Connect button input straight to the green led
+            m.d.comb += rgb_led.g.o.eq(btn.i)
+        elif xdr == 2:
+            # Rising edge  => green
+            # Falling edge => blue
+            m.d.comb += rgb_led.g.o.eq(btn.i0)
+            m.d.comb += rgb_led.b.o.eq(btn.i1)
 
         return m
 
 if __name__ == "__main__":
     p = TangNanoPlatform()
     # p.build(BlinkyXDROut(xdr=0), do_program=True)
-    p.build(BlinkyXDROut(xdr=1), do_program=True)
+    # p.build(BlinkyXDROut(xdr=1), do_program=True)
     # p.build(BlinkyXDROut(xdr=2), do_program=True)
 
     # p.build(BlinkyXDR10Out(), do_program=True)
 
     # p.build(BlinkyXDRIn(xdr=0), do_program=True)
     # p.build(BlinkyXDRIn(xdr=1), do_program=True)
-    # p.build(BlinkyXDRIn(xdr=2), do_program=True)
+    p.build(BlinkyXDRIn(xdr=2), do_program=True)
 
